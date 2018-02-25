a5f0538d5558f37c7afd94491c793a3ad9babd64
eugordd <eugord@yandex.ru>
2018-02-25 13:55:09 +0300

    generate hmac string

--- bot-2.js
+++ bot-2.js
@@ -1,14 +1,31 @@
 const axios = require('axios')
+const crypto = require("crypto");
+const moment = require("moment");
 
+const key = '09B064DBD34EEEB07DABBF230E4A5B22'
+const secret = '590e5f84dd2a8cdaf3036b14254b8b28'
+const payload = {'nonce': 125}
 
+function hmacPayload (secret, payload) {
+  const string = Object.keys(payload)
+    .sort()
+    .map(k => `${k}=${payload[k]}`)
+    .join('\n')
+  return crypto.createHmac('sha512', secret)
+          .update(string)
+          .digest('hex')
+}
 
-axios({
-  method: 'post',
-  url: 'https://yobit.net/tapi/?getInfo&nonce=100',
-  headers: {
-    'Key': '09B064DBD34EEEB07DABBF230E4A5B22',
-    'Sign': '590e5f84dd2a8cdaf3036b14254b8b28',
-    "Content-type": "application/x-www-form-urlencoded",
-  }
-}).then(res => console.log(res))
-.catch(e => console.log(e))
+var hmac = hmacPayload(secret, payload)
+console.log(hmac)
+
+// axios({
+//   method: 'getInfo',
+//   url: 'https://yobit.net/tapi/',
+//   headers: {
+//     'Key': key,
+//     'Sign': hmac,
+//     "Content-type": "application/x-www-form-urlencoded",
+//   }
+// }).then(res => console.log(res))
+// .catch(e => console.log(e))
