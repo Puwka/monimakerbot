ad5f5be0f559699ba25b9c49706e957e4d58c7a6
Puwka <gorokhov.inc@gmail.com>
2018-02-25 16:40:09 +0300

    rebuild storage

--- localData/localData.js
+++ localData/localData.js
@@ -1,9 +1,16 @@
-const key = '123'
-const secret = '321'
+const fs = require('fs')
 
-let getKey = () => key
+let getKey = () => {
+  let storage = fs.readFileSync('./keys.json')
+  let result = JSON.parse(storage)
+  return result.key
+}
 
-let getSecret = () => secret
+let getSecret = () => {
+  let storage = fs.readFileSync('./keys.json')
+  let result = JSON.parse(storage)
+  return result.secret
+}
 
 module.exports = {
   getKey,
