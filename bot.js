4b295a644498b030099f50475557da1c4772e00c
Puwka <gorokhov.inc@gmail.com>
2018-02-25 10:23:08 +0300

    first com

---
+++ bot.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const fs= require('fs')
+
+let vp = 'epy_btc'
+
+setInterval(() => {
+  axios.get(`https://yobit.net/api/3/trades/${vp}`)
+  .then(response => {
+    if (!fs.existsSync(`./${vp}_trades.txt`)) {
+      fs.writeFile(`${vp}_trades.txt`, `Price: ${JSON.stringify(response.data[vp][0].price)} Timestamp: ${JSON.stringify(response.data[vp][0].timestamp)} Amount: ${JSON.stringify(response.data[vp][0].amount)}` + `\n\n`, e => {
+        if(e) throw e
+      })
+    } else {
+      fs.appendFile(`${vp}_trades.txt`, `Price: ${JSON.stringify(response.data[vp][0].price)} Timestamp: ${JSON.stringify(response.data[vp][0].timestamp)} Amount: ${JSON.stringify(response.data[vp][0].amount)}` + '\n\n', e => {
+        if(e) throw e
+      })
+    }
+  })
+  .catch(e => console.log(e))
+}, 1200)
+
+setInterval(() => {
+  axios.get(`https://yobit.net/api/3/ticker/${vp}`)
+  .then(response => {
+    if (!fs.existsSync(`./${vp}_ticker.txt`)) {
+      fs.writeFile(`${vp}_ticker.txt`, `Buy: ${JSON.stringify(response.data[vp].buy)} Sell: ${JSON.stringify(response.data[vp].sell)}` + `\n\n`, e => {
+        if(e) throw e
+      })
+    } else {
+      fs.appendFile(`${vp}_ticker.txt`, `Buy: ${JSON.stringify(response.data[vp].buy)} Sell: ${JSON.stringify(response.data[vp].sell)}` + '\n\n', e => {
+        if(e) throw e
+      })
+    }
+  })
+  .catch(e => console.log(e))
+}, 1200)
