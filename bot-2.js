68af73a99d2d16222b9b37359c85d3cc584fa428
Puwka <gorokhov.inc@gmail.com>
2018-02-25 15:56:35 +0300

    local keys

--- bot-2.js
+++ bot-2.js
@@ -1,10 +1,14 @@
+// 3rd party
 const axios = require('axios')
+// node.js pack
 const crypto = require("crypto");
-const moment = require("moment");
+//project modules
+const localData = require('./localData/localData')
+// const moment = require("moment");
 
-const key = '09B064DBD34EEEB07DABBF230E4A5B22'
-const secret = '590e5f84dd2a8cdaf3036b14254b8b28'
-const payload = {'nonce': 125}
+const key = localData.getKey()
+const secret = localData.getSecret()
+const payload = {'nonce': 129}
 
 function hmacPayload (secret, payload) {
   const string = Object.keys(payload)
@@ -19,13 +23,17 @@ function hmacPayload (secret, payload) {
 var hmac = hmacPayload(secret, payload)
 console.log(hmac)
 
-// axios({
-//   method: 'getInfo',
-//   url: 'https://yobit.net/tapi/',
-//   headers: {
-//     'Key': key,
-//     'Sign': hmac,
-//     "Content-type": "application/x-www-form-urlencoded",
-//   }
-// }).then(res => console.log(res))
-// .catch(e => console.log(e))
+axios({
+  method: 'post',
+  url: `https://yobit.net/tapi/`,
+  headers: {
+    'Key': key,
+    'Sign': hmac,
+    "Content-type": "application/x-www-form-urlencoded",
+  },
+  data: {
+    nonce: 129,
+    method: 'getInfo'
+  }
+}).then(res => console.log(res))
+.catch(e => console.log(e))
---
+++ localData/localData.js
@@ -0,0 +1,15 @@
+const key = '123'
+const secret = '321'
+
+let getKey = () => {
+  return key
+}
+
+let getSecret = () => {
+  return secret
+}
+
+module.exports = {
+  getKey,
+  getSecret
+}
